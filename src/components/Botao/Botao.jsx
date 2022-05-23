946604e88a995568a7eb68d42322de107dc4cfa9
unknown <rwps1234@gmail.com>
2022-05-23 00:11:35 -0300

    adicionando evento onClick

--- src/components/Botao/Botao.jsx
+++ src/components/Botao/Botao.jsx
@@ -1,6 +1,6 @@
 import styles from "./Botao.module.css"
-export default function Botao ({texto}) {
+export default function Botao ({texto, onClick}) {
     return (
-        <button className={`${styles.botoes} ${styles.largura}`}>{texto}</button>
+        <button className={`${styles.botoes} ${styles.largura}`} onClick={onClick}>{texto}</button>
     )
 }
