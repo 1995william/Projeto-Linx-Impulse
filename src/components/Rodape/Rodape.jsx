b16e7b54f1cb836798b7306c83d776ac9518af98
unknown <rwps1234@gmail.com>
2022-05-23 23:13:38 -0300

    inserindo classes

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -1,11 +1,13 @@
+import styles from "./Rodape.module.css";
+
 export default function Rodape() {
   return (
-    <footer>
-      <article>
+    <footer className={styles.rodapeContainer}>
+      <section>
         <p>Testando suas habilidades em HTML, CSS e JS.</p>
         <p>Linx Impulse</p>
         <p>2022</p>
-      </article>
+      </section>
     </footer>
   );
 }
