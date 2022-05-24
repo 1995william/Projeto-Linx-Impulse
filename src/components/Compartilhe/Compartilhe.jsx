d196a960c043b7a79489e16ffd09e0bfa9e0b524
unknown <rwps1234@gmail.com>
2022-05-23 23:11:47 -0300

    adicionando classes

--- src/components/Compartilhe/Compartilhe.jsx
+++ src/components/Compartilhe/Compartilhe.jsx
@@ -1,16 +1,31 @@
-import Botao from "../Botao/Botao"
-export default function Compartilhe () {
-    return (
-        <section>
-            <h1>Compartilhe a novidade</h1>
-            <div>
-                <p>Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!</p>
-            </div>
-            <div>
-                <label>Nome do seu amigo: <input type="text" /></label>
-                <label>E-mail: <input type="text" /></label>
-            </div>
-            <Botao texto='Enviar agora'/>
-        </section>
-    )
+import Botao from "../Botao/Botao";
+import styles from "./Compartilhe.module.css";
+import Divisor from "../Divisor/Divisor";
+
+export default function Compartilhe() {
+  return (
+    <section className={styles.secaoContainer}>
+      <Divisor texto="Compartilhe a novidade" />
+
+      <div className={styles.secao}>
+        <aside>
+          <p>
+            Quer que seus amigos também ganhem a lista personalizada deles?
+            Preencha agora!
+          </p>
+        </aside>
+        <div className={styles.labelInput}>
+          <label>
+            Nome do seu amigo: <br /> <input type="text" />
+          </label>
+          <label>
+            E-mail: <br /> <input type="email" />
+          </label>
+        </div>
+      </div>
+      <div className={styles.btnEnviar}>
+        <Botao texto="Enviar agora" />
+      </div>
+    </section>
+  );
 }
