8da24a781ceafa353c3c5ef6b52087ba14ad5703
unknown <rwps1234@gmail.com>
2022-05-25 21:48:26 -0300

    validando Formulário

--- src/components/AjudeAlgoritimo/Formulario/Formulario.jsx
+++ src/components/AjudeAlgoritimo/Formulario/Formulario.jsx
@@ -1,28 +1,82 @@
 import Botao from "../../Botao/Botao";
 import styles from "./Formulario.module.css";
+import { useState } from "react";
 export default function Formulario() {
+  
+  const [user, setUser] = useState({
+    name:'',
+    email:'',
+    cpf:''
+  })
+   const [status, setStatus] = useState({
+    type:'',
+    mensagem:'',
+  })
+ const valueInput = e => setUser({...user,[e.target.name]: e.target.value});
+
+ const cadastro = e => {
+
+    e.preventDefault();
+
+    if(!validate()) return;
+
+    const saveDataForm = true;
+    if(saveDataForm) {
+      setStatus({
+        type: 'successo',
+        mensagem: 'Usuário cadastrado com sucesso!'
+      })
+      setUser({
+        name: '',
+        email:'',
+        cpf: ''
+      })
+    } else {
+      setStatus({
+        type: 'erro',
+        mensagem: 'Erro: Usuário nao cadastrado com sucesso!'
+      })
+    }
+  }
+
+  function validate() {
+    if (!user.name) return setStatus({ type: 'erro', mensagem: 'Preencha o campo nome' });
+    if (!user.email) return setStatus({ type: 'erro', mensagem: 'Preencha o campo Email' });
+    if (!user.cpf) return setStatus({ type: 'erro', mensagem: 'Preencha o campo CPF' });
+    if (user.cpf.match(/^[0-9]*$/) == null) return setStatus({ type: 'erro', mensagem: 'Digite apenas números no campo CPF' });
+    if (user.cpf.length < 11 | user.cpf.length > 11) return setStatus({ type: 'erro', mensagem: 'CPF precisa ter 11 dígitos' });
+
+    return true;
+  }
   return (
-    <form className={styles.formulario}>
-      
+    <form className={styles.formulario} onSubmit={cadastro}>
+      {status.type == 'successo'? <small className={styles.msgSucesso}>{status.mensagem}</small>: ''}
+      {status.type == 'erro'? <small className={styles.msgErro}>{status.mensagem}</small>: ''}
       <label>
-        Seu nome: <br/>
-        <input className={styles.inputTexto} type="text" />
+        Seu nome: <br />
+        <input className={styles.inputTexto} name="name" type="text" onChange={valueInput} value={user.name}/>
       </label>
 
       <label>
-        E-mail <br/>
-        <input className={styles.inputTexto} type="email" />
+        E-mail <br />
+        <input className={styles.inputTexto} name="email" type="email" onChange={valueInput} value={user.email}/>
       </label>
 
       <label>
-        CPF: <br/>
-        <input className={styles.inputTexto} type="text" />
+        CPF: <br />
+        <input
+          className={styles.inputTexto}
+          minLength={11}
+          name="cpf"
+          type="text"
+          onChange={valueInput}
+          value={user.cpf}
+        />
       </label>
-      
-      
+
       <div className={styles.containerLabel}>
         <label className={styles.labelRadio}>
-          <input className={styles.radio} name="sexo" type="radio" />
+          <input className={styles.radio} name="sexo" type="radio" checked />
           Masculino
         </label>
 
@@ -32,7 +86,7 @@ export default function Formulario() {
         </label>
       </div>
       <div className={styles.btnEnviar}>
-        <Botao texto="Enviar" />
+        <Botao texto="Enviar" type="submit" />
       </div>
     </form>
   );
